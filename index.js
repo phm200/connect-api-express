03aff0f949f9280faeadf1d69477cfdce7338fdc
Peter Miller <peter.h.miller@gmail.com>
2018-09-27 17:25:07 -0400

    Initial commit of Express app that can get users and update contact attributes using the Connect API

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const AWS = require("aws-sdk");
+require("express-async-errors");
+
+const connectInstanceId = "--put your connect instance id here--";
+
+var connectClient = new AWS.Connect({
+  apiVersion: "2017-08-08",
+  region: "us-east-1"
+});
+
+// serve static content from public directory
+app.use(express.static("public"));
+
+// handle form data
+app.use(express.urlencoded({ extended: true }));
+
+// configuration for pug views
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+// index - list of users
+app.get("/", async (req, res) => {
+  var listUsersParams = {
+    InstanceId: connectInstanceId
+  };
+  var listUsersPromise = connectClient.listUsers(listUsersParams).promise();
+  var listUsersResponse = await listUsersPromise;
+  res.render("index", {
+    title: "Connect Users",
+    dataList: listUsersResponse.UserSummaryList
+  });
+});
+
+// user detail by user id
+app.get("/user/:userId", async (req, res) => {
+  var userId = req.params.userId;
+  var describeUserParams = {
+    InstanceId: connectInstanceId,
+    UserId: userId
+  };
+  var describeUserPromise = connectClient
+    .describeUser(describeUserParams)
+    .promise();
+  var describeUserResult = await describeUserPromise;
+  res.render("user", {
+    title: describeUserResult.User.Username,
+    user: describeUserResult.User
+  });
+});
+
+app.get("/updateAttributes", async (req, res) => {
+  res.render("updateAttributes", {
+    title: "Update Contact Attributes"
+  });
+});
+
+app.post("/submit-updateAttributes", async (req, res) => {
+  const contactId = req.body.contactId;
+  const flagForFollowUpRaw = req.body.flagForFollowUp; // will equal "on" if checked, undefined if false
+  const flagForFollowUp = flagForFollowUpRaw && flagForFollowUpRaw === "on";
+
+  var updateContactAttributesParams = {
+    InstanceId: connectInstanceId,
+    InitialContactId: contactId,
+    Attributes: {
+      FlaggedForFollowUp: flagForFollowUp.toString()
+    }
+  };
+
+  var updateContactAttributesPromise = connectClient
+    .updateContactAttributes(updateContactAttributesParams)
+    .promise();
+  var updateContactAttributesResult = await updateContactAttributesPromise;
+  console.log("result", updateContactAttributesResult);
+
+  res.render("submittedUpdateAttributes", {
+    title: "Contact Attributes Updated"
+  });
+});
+
+app.listen(3000, () => console.log("App listening on port 3000!"));
