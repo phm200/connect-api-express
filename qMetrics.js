082f3d15b707d42e9db261df48565cc1ab7f66ad
Peter Miller <peter.h.miller@gmail.com>
2018-10-01 14:24:28 -0400

    Adding current queue metrics page

---
+++ qMetrics.js
@@ -0,0 +1,50 @@
+const metricsList = [
+  {
+    Name: "AGENTS_AVAILABLE",
+    Unit: "COUNT"
+  },
+  {
+    Name: "AGENTS_ONLINE",
+    Unit: "COUNT"
+  },
+  {
+    Name: "AGENTS_ON_CALL",
+    Unit: "COUNT"
+  },
+  {
+    Name: "AGENTS_ONLINE",
+    Unit: "COUNT"
+  },
+  {
+    Name: "AGENTS_STAFFED",
+    Unit: "COUNT"
+  },
+  {
+    Name: "AGENTS_AFTER_CONTACT_WORK",
+    Unit: "COUNT"
+  },
+  {
+    Name: "AGENTS_NON_PRODUCTIVE",
+    Unit: "COUNT"
+  },
+  {
+    Name: "AGENTS_ERROR",
+    Unit: "COUNT"
+  },
+  {
+    Name: "CONTACTS_IN_QUEUE",
+    Unit: "COUNT"
+  },
+  {
+    Name: "OLDEST_CONTACT_AGE",
+    Unit: "SECONDS"
+  },
+  {
+    Name: "CONTACTS_SCHEDULED",
+    Unit: "COUNT"
+  }
+];
+
+module.exports = {
+  metricsList: metricsList
+};
